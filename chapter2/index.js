1b6996d72aef8f84e59a747131ffce65a7c973af
shigaki daichi <d-shigaki@pro-seeds.co.jp>
2019-03-30 17:57:17 +0900

    wip

--- chapter2/index.js
+++ chapter2/index.js
@@ -1,45 +1,45 @@
-  // child cmp parts
-  var counterButton = Vue.extend({
+// child cmp parts
+var counterButton = Vue.extend({
     template: '<span>{{ counter }}個 <button v-on:click="addToCart">追加</button></span>',
-      data: function() {
-      return {
-        counter: 0
-      }
-    },
-    methods : {
-      addToCart: function() {
-        this.counter += 1
-        this.$emit('increment') // incrementカスタムイベントの発火
-      }
-    },
-  })
-
-  // child cmp
-  new Vue({
-    el: '#fruits-counter',
-    components: {
-      'counter-button': counterButton
-    },
-    data: {
-      total: 0,
-      fruits: [
-        {name: '梨'},
-        {name: 'いちご'}
-      ]
+    data: function () {
+        return {
+            counter: 0
+        }
     },
     methods: {
-      incrementCartStatus: function() {
-        this.total += 1
-      }
+        addToCart: function () {
+            this.counter += 1
+            this.$emit('increment') // incrementカスタムイベントの発火
+        }
+    },
+})
+
+// child cmp
+new Vue({
+    el: '#fruits-counter',
+    components: {
+        'counter-button': counterButton
+    },
+    data: {
+        total: 0,
+        fruits: [
+            { name: '梨' },
+            { name: 'いちご' }
+        ]
+    },
+    methods: {
+        incrementCartStatus: function () {
+            this.total += 1
+        }
     }
-  })
+})
 
-    Vue.component('list-item', {
-      template: '<li>foo</li>',
-      data: function() {
+// another cmp
+Vue.component('list-item', {
+    template: '<li>foo</li>',
+    data: function () {
         return { content: 'bar' }
-      }
-    })
+    }
+})
 
-    new Vue({ el: '#example' })
-    
+new Vue({ el: '#example' })
